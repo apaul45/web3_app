e89f5ec4ecf7af001e2ffd8ffc3557482ce88f6d
Ayon Paul <ayon.paul@stonybrook.edu>
2023-11-21 19:24:21 -0500

    Add create function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.13;
 
 contract TodoList {
   uint public taskCount = 0; // State variable that's stored on blockchain, public keyword provides readonly fn
+  
+  constructor() public {
+    createTask("Create new task");
+  }
+    
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+  
+  mapping(uint => Task) public tasks;
+  
+  function createTask(string memory _conent) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _conent, false);
+  }
 }
